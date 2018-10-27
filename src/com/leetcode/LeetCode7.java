99a580ad147dc0f6dcb119ff2d7f5769786dff0f
TeacherCangNulling <3218870799@qq.com>
2018-10-27 20:42:06 +0800

    Leetcode7

---
+++ src/com/leetcode/LeetCode7.java
@@ -0,0 +1,51 @@
+package com.leetcode;
+
+public class LeetCode7 {
+	
+	public static void main(String[] args) {
+		LeetCode7 leetcode = new LeetCode7();
+		LeetCode7.Solution solution = leetcode.getSolution();
+		int result = solution.reverse(123);
+		System.out.println(result);
+	}
+	
+	public Solution getSolution(){
+		return new Solution();
+	}
+	
+	class Solution {
+	    public int reverse(int x) {
+	        int rev = 0;
+	        while (x != 0) {
+	            int lastbit = x % 10;
+	            x /= 10;
+	            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && lastbit > 7)) return 0;
+	            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && lastbit < -8)) return 0;
+	            rev = rev * 10 + lastbit;
+	        }
+	        return rev;
+	    }
+	}
+	class BestSolution {
+		public int reverse(int x) {
+			int max = Integer.MAX_VALUE / 10;
+			int min = Integer.MIN_VALUE / 10;
+			int maxBit = Integer.MAX_VALUE % 10;
+			int minBit = Integer.MIN_VALUE % 10;
+			int result = 0;
+			while (x != 0) {
+				int lastBit = x % 10;
+				x /= 10;
+				if (result > max || (result == max && lastBit > maxBit)) {
+					return 0;
+				}
+				if (result < min || (result == min && lastBit < minBit)) {
+					return 0;
+				}
+				result = result * 10 + lastBit;
+			}
+			return result;
+		}
+	}
+	
+}
