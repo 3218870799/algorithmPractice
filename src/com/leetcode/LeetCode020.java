4a01c3e1422ea38173e34761a76660a967238f00
TeacherCangNulling <3218870799@qq.com>
2018-11-14 16:39:39 +0800

    有效地括号

---
+++ src/com/leetcode/LeetCode020.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+import java.util.List;
+
+public class LeetCode020 {
+	
+	public static void main(String[] args) {
+		LeetCode020 leetcode = new LeetCode020();
+		LeetCode020.Solution solution = leetcode.getSolution();
+		String s = "()[]{}";
+		boolean result = solution.isValid(s);
+		
+		System.out.println(result);
+	}
+
+	public Solution getSolution(){
+		return new Solution();
+	}
+	
+	class Solution {
+	    public boolean isValid(String s) {
+	        char[] stack = new char[s.length() + 1];
+	        int top = 1;
+	        for (char c : s.toCharArray()) {
+	            if (c == '(' || c == '[' || c == '{') {
+	                stack[top++] = c; 
+	            } else if (c == ')' && stack[--top] != '(') {
+	                return false;
+	            } else if (c == ']' && stack[--top] != '[') {
+	                return false;
+	            } else if (c == '}' && stack[--top] != '{') {
+	                return false;
+	            }
+	        }
+	        return top == 1;
+	    }
+	}
+}
