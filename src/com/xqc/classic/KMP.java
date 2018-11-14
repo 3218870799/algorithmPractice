0ef615c554a0b8d5f4d4a9b2825afd924bd52044
TeacherCangNulling <3218870799@qq.com>
2018-11-14 21:24:57 +0800

    KMP算法

---
+++ src/com/xqc/classic/KMP.java
@@ -0,0 +1,62 @@
+package com.xqc.classic;
+
+public class KMP {
+	
+	public static void main(String[] args) {
+		String str = "abcabcababaccc";
+		String match = "ababa";
+		System.out.println(getIndexOf(str, match));
+	}
+	
+	public static int getIndexOf(String s,String m){
+		if(s==null||m==null||m.length()<1||s.length()<m.length()){
+			return -1;
+		}
+		char[] str1 = s.toCharArray();
+		char[] str2 = m.toCharArray();
+		int i1 = 0;
+		int i2 = 0;
+		int[] next = getNextArray(str2);
+		
+		while(i1<str1.length && i2<str2.length){
+			if(str1[i1]==str2[i2]){
+				i1++;
+				i2++;
+			}else if(next[i2]==-1){
+				i1++;
+			}else{
+				i2 = next[i2];
+			}
+		}
+		return i2 == str2.length ? i1-i2 : -1;
+	}
+
+	private static int[] getNextArray(char[] str) {
+		if(str.length == 1){
+			return new int[] {-1};
+		}
+		int[] next = new int[str.length];
+		next[0]=-1;
+		next[1]=0;
+		//
+		int i = 2;
+		//Ìøµ½µÄÎ»ÖÃ
+		int cn = 0;
+		while(i<next.length){
+			if(str[i-1]==str[cn]){
+				next[i++] = ++cn;
+			}else if(cn>0){
+				cn = next[cn];
+			}else{
+				next[i++]=0;
+			}
+		}
+/*		for(int j :next){
+			System.out.println(j);
+		}*/
+		return next;
+	}
+	
+	
+
+}
