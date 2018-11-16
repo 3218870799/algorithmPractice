e33abf71141850c608878edfe9f927113f1e894a
TeacherCangNulling <3218870799@qq.com>
2018-11-16 21:09:34 +0800

    两两交换链表中节点

---
+++ src/com/leetcode/LeetCode024.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+import java.util.List;
+
+import com.leetcode.LeetCode023.ListNode;
+
+public class LeetCode024 {
+	
+	public static void main(String[] args) {
+		LeetCode024 leetcode = new LeetCode024();
+		LeetCode024.Solution solution = leetcode.getSolution();
+	}
+
+	public Solution getSolution(){
+		return new Solution();
+	}
+	public class ListNode {
+	     int val;
+	     ListNode next;
+	     ListNode(int x) { val = x; }
+	 }
+	
+	
+	class Solution {
+	    public ListNode swapPairs(ListNode head) {
+	        ListNode preHead = new ListNode(0), cur = preHead;
+	        preHead.next = head;
+	        while (cur.next != null && cur.next.next != null) {
+	            ListNode temp = cur.next.next;
+	            cur.next.next = temp.next;
+	            temp.next = cur.next;
+	            cur.next = temp;
+	            cur = cur.next.next;
+	        }
+	        return preHead.next;
+	    }
+	}
+}
