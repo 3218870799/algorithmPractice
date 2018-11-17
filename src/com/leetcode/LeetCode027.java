9914c81feb89c964543c683aa75bbe9652e4104d
TeacherCangNulling <3218870799@qq.com>
2018-11-17 17:40:05 +0800

    移除元素

---
+++ src/com/leetcode/LeetCode027.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+/**
+ *
+ * 数组
+ * 移除指定元素
+ *
+ */
+public class LeetCode027 {
+	
+	public static void main(String[] args) {
+		LeetCode027 leetcode = new LeetCode027();
+		LeetCode027.Solution solution = leetcode.getSolution();
+		
+		int[] nums = {2,2,3};
+		int len = solution.removeElement(nums, 3);
+		
+		for(int i=0;i<len;i++){
+			System.out.println(nums[i]);
+		}
+	}
+
+	public Solution getSolution(){
+		return new Solution();
+	}
+	
+	class Solution {
+	    public int removeElement(int[] nums, int val) {
+	        int tail = 0;
+	        for (int i = 0, len = nums.length; i < len; ++i) {
+	            if (nums[i] != val) {
+	                nums[tail++] = nums[i];
+	            }
+	        }
+	        return tail;
+	    }
+	}
+}
