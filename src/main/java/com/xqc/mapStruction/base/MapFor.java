b49b0daa9d904db9c99d60dafee57041bb3b86c1
TeacherCangNulling <3218870799@qq.com>
2020-11-19 09:27:50 +0800

    map的遍历的几种方式

---
+++ src/main/java/com/xqc/mapStruction/base/MapFor.java
@@ -0,0 +1,41 @@
+package com.xqc.mapStruction.base;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MapFor {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("1", "value1");
+        map.put("2", "value2");
+        map.put("3", "value3");
+
+        //第一种：普遍使用，由于二次取值,效率会比第二种和第三种慢一倍
+        System.out.println("通过Map.keySet遍历key和value：");
+        for (String key : map.keySet()) {
+            System.out.println("key= "+ key + " and value= " + map.get(key));
+        }
+        //第二种
+        System.out.println("通过Map.entrySet使用iterator遍历key和value：");
+        Iterator<Map.Entry<String, String>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, String> entry = it.next();
+            System.out.println("key= " + entry.getKey() + " and value= " + entry.getValue());
+        }
+
+        //第三种：无法在for循环时实现remove等操作
+        System.out.println("通过Map.entrySet遍历key和value");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println("key= " + entry.getKey() + " and value= " + entry.getValue());
+        }
+
+        //第四种：只能获取values,不能获取key
+        System.out.println("通过Map.values()遍历所有的value，但不能遍历key");
+        for (String v : map.values()) {
+            System.out.println("value= " + v);
+        }
+
+
+    }
+}
