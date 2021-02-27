501810defb4a72bfbf3bb255964be90e6f1b2af4
TeacherCangNullering <3218870799@qq.com>
2021-02-27 15:57:41 +0800

    LeetCode25k个一组翻转链表

---
+++ src/main/java/com/leetcode/linkedList/LeetCode25ReverseKGroup.java
@@ -0,0 +1,57 @@
+package com.leetcode.linkedList;
+
+public class LeetCode25ReverseKGroup {
+
+    public ListNode reverseKGroup2(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) end = end.next;
+            if (end == null) break;
+            ListNode start = pre.next;
+            ListNode nextGroup = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = nextGroup;
+            pre = start;
+
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+}
